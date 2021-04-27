f41b5ddeb14ebd8fe249a5abb1310c8883493b08
Johannes Schüth <git@jotschi.de>
2021-04-27 18:47:48 +0200

    Setup newest dagger

--- containers/server/src/main/java/io/metaloom/loom/container/server/ServerRunner.java
+++ containers/server/src/main/java/io/metaloom/loom/container/server/ServerRunner.java
@@ -1,9 +1,12 @@
 package io.metaloom.loom.container.server;
 
+import io.metaloom.loom.core.dagger.DaggerLoomComponent;
+import io.metaloom.loom.core.dagger.LoomComponent;
+
 public class ServerRunner {
 
 	public static void main(String[] args) {
-		
+		LoomComponent loomComponent = DaggerLoomComponent.builder().build();
 	}
 
 }
